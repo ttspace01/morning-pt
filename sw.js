4fc0f841bff02d7f0b25680f14a3725fedee07b5
ttspace01 <ttspace01@gmail.com>
2026-07-13 23:33:23 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'morning-pt-v1';
+const SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => {
+      if (hit) return hit;
+      return fetch(e.request).then(res => {
+        // cache the Google Fonts files on first run so the app works offline after
+        if (res.ok && e.request.url.includes('fonts.g')) {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => hit);
+    })
+  );
+});
